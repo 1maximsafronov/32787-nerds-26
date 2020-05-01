22f436ed72dbe3ff4f76fffaaefe616d9ce7c8f3
Maxim Safronov <1maximsafronov@gmail.com>
2020-05-02 00:51:06 +0700

    исправил скрипт открытия/закрытия модального окна

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,19 @@
-var writeUsBtn = document.querySelector('.contacts__write-us');
-var writeUsModal = document.querySelector('.modal-write-us');
-var modalClose = writeUsModal.querySelector('.modal__close');
+const writeUsBtn = document.querySelector('.contacts__write-us');
+const writeUsModal = document.querySelector('.modal-write-us');
+const modalClose = writeUsModal.querySelector('.modal__close');
 
-writeUsBtn.addEventListener('click', function(evnt){
-    evnt.preventDefault();
+writeUsBtn.addEventListener('click', function(evt){
+    evt.preventDefault();
     writeUsModal.classList.add('modal--show');
 });
 
-modalClose.addEventListener('click', function(evnt){
-    evnt.preventDefault();
+modalClose.addEventListener('click', function(evt){
+    evt.preventDefault();
     writeUsModal.classList.remove('modal--show');
 });
-document.addEventListener('keydown', function(evnt){
-  if(evnt.keycode = 27) {
+document.addEventListener('keydown', function(evt){
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
     writeUsModal.classList.remove('modal--show');
   }
 });
