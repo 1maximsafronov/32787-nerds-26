c7b9179fcbbc06c1cda227524d37fd785cd57df5
1maximsafronov <1maximsafronov@gmail.com>
2020-05-03 20:23:32 +0700

    Добавил смену активного стостояния у направления сортировки

--- src/js/sortmenu.js
+++ src/js/sortmenu.js
@@ -1,5 +1,11 @@
 (function () {
-  let isSortUp = false;
+  const SortType = {
+    BY_PRICE: 'price',
+    BY_TYPE: 'layout',
+    BY_NAME: 'title'
+  };
+
+  let isSortUp = true;
   let activSortType = '';
   const sortingmenu = document.querySelector('.catalog__sort');
   const toggleByPrice = sortingmenu.querySelector('.sorting__item--by-price');
@@ -8,61 +14,69 @@
   const toggleDown = sortingmenu.querySelector('.sorting__toggle--down');
   const toggleUp = sortingmenu.querySelector('.sorting__toggle--up');
 
+  toggleByPrice.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    toggleActiveSortType(SortType.BY_PRICE);
+    toggleActiveSortingItem(toggleByPrice);
+    renderSorteredProducts();
+  });
+
+  toggleByType.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    toggleActiveSortType(SortType.BY_TYPE);
+    toggleActiveSortingItem(toggleByType);
+    renderSorteredProducts();
+  });
+
+  toggleByName.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    toggleActiveSortType(SortType.BY_NAME);
+    toggleActiveSortingItem(toggleByName);
+    renderSorteredProducts();
+  });
+
   toggleDown.addEventListener('click', function (evt) {
     evt.preventDefault();
     let products = window.catalog.getLoadedProducts();
     isSortUp = false;
-    window.catalog.render(sortProducts(products, activSortType));
+    renderSorteredProducts();
   });
 
   toggleUp.addEventListener('click', function (evt) {
     evt.preventDefault();
     let products = window.catalog.getLoadedProducts();
     isSortUp = true;
-    window.catalog.render(sortProducts(products, activSortType));
+    renderSorteredProducts();
   });
 
-  toggleByName.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    activSortType = 'title';
-    let products = window.catalog.getLoadedProducts();
-    window.catalog.render(sortProducts(products, activSortType));
-    isSortUp = !isSortUp;
-    toggleActiveSortingItem(toggleByName);
-  });
-
-  toggleByType.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    activSortType = 'layout';
-    let products = window.catalog.getLoadedProducts();
-    window.catalog.render(sortProducts(products, activSortType));
-    isSortUp = !isSortUp;
-    toggleActiveSortingItem(toggleByType);
-  });
-
-  toggleByPrice.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    activSortType = 'price';
-    let products = window.catalog.getLoadedProducts();
-    window.catalog.render(sortProducts(products, activSortType));
-    isSortUp = !isSortUp;
-    toggleActiveSortingItem(toggleByPrice);
-  });
-
+  function toggleActiveSortType(sortType) {
+    if(activSortType === sortType) {
+      isSortUp = !isSortUp;
+    }
+    activSortType = sortType;
+  }
   function toggleActiveSortingItem(sortinItem) {
     let activeSortinItem = sortingmenu.querySelector('.sorting__item--active');
-
     activeSortinItem.classList.remove('sorting__item--active');
     sortinItem.classList.add('sorting__item--active');
   }
 
-  // function toggleActiveSortingToggler(sortinToggler) {
-  //   let activeSortinItem = sortingmenu.querySelector('.sorting__item--active');
-  //
-  //   activeSortinItem.classList.remove('sorting__item--active');
-  //   sortinToggler.classList.add('sorting__item--active');
-  // }
+  function toggleActiveSortingToggler() {
+    let activeSortinItem = sortingmenu.querySelector('.sorting__toggle--active');
+    activeSortinItem.classList.remove('sorting__toggle--active');
+    if(isSortUp) {
+      toggleUp.classList.add('sorting__toggle--active');
+    }
+    else {
+      toggleDown.classList.add('sorting__toggle--active');
+    }
+  }
 
+function renderSorteredProducts() {
+  let products = window.catalog.getLoadedProducts();
+  window.catalog.render(sortProducts(products, activSortType));
+  toggleActiveSortingToggler();
+}
   function sortProducts(products, value) {
     const length = products.length;
     for (var i = 0; i < length-1; i++){
